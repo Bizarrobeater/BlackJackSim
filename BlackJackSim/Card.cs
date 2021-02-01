12be20e20668c0a44f3b7fd6c94bff810f885f5d
Christopher Lind <Christopher.lind.c@gmail.com>
2021-02-01 13:53:08 +0100

    Added enum for cards

---
+++ BlackJackSim/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackSim
+{
+    public enum Card
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    public enum Suit
+    {
+        Spade,
+        Hearts,
+        Clubs,
+        Diamonds
+    }
+
+    public enum Color
+    {
+        Black,
+        Red
+    }
+}
---
+++ BlackJackSim/ICard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackSim
+{
+    public interface ICard
+    {
+        public int GetValue();
+        public Card GetFace();
+        public bool IsRevealed();
+    }
+}
